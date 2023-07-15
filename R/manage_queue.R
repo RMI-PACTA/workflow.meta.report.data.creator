4191d4b7c49095584ae4b60becd533cf555e280e
Alex Axthelm <aaxthelm@RMI.org>
2023-07-15 17:44:32 +0200

    Revert to using txtq

--- R/manage_queue.R
+++ R/manage_queue.R
@@ -1,3 +1,19 @@
+queue_lock_file <- function(queue){
+  path <- file.path(normalizePath(queue$path()), "lock")
+  if (!file.exists(path)) {
+    file.create(path)
+  }
+  return(path)
+}
+
+supplemental_file <- function(queue){
+  path <- file.path(normalizePath(queue$path()), "supplemental")
+  if (!file.exists(path)) {
+    file.create(path)
+  }
+  return(path)
+}
+
 prepare_queue_message <- function(
   relpath,
   portfolio_name_ref_all,
@@ -18,6 +34,17 @@ prepare_queue_message <- function(
   )
 }
 
+write_supplemental <- function(contents, queue) {
+  supplemental_file <- supplemental_file(queue)
+  on.exit(filelock::unlock(lock))
+  lock <- filelock::lock(queue_lock_file(queue))
+  write(
+    x = contents,
+    file = supplemental_file,
+    append = TRUE
+  )
+}
+
 parse_queue_message <- function(message){
   x <- strsplit(message, split = ",", fixed = TRUE)
   m <- lapply(X =x, FUN = matrix, byrow = TRUE, ncol = 6)
@@ -33,16 +60,63 @@ parse_queue_message <- function(message){
   return(out)
 }
 
-interrogate_queue <- function(queue_db, table_name = "qqportfolio"){
-  con <- DBI::dbConnect(
-    RSQLite::SQLite(),
-    queue_db
+read_supplemental <- function(queue, skip = 0, n = Inf){
+  nmax = ifelse(identical(n, Inf), -1, n)
+  lock <- filelock::lock(queue_lock_file(queue))
+  raw_contents <- scan(
+    file = supplemental_file(queue),
+    skip = skip,
+    what = character(),
+    nmax = nmax,
+    # using a sep that shouldn't appear in the contents
+    sep = "|",
+    na.strings = NULL,
+    quiet = TRUE
   )
-  queue_table <- dplyr::collect(
-    dplyr::tbl(con, table_name)
+  filelock::unlock(lock)
+  out <- parse_queue_message(raw_contents)
+  return(out)
+}
+
+find_runtime <- function(data){
+  relpath <- unique(data$relpath)
+  portfolio_name_ref_all <- unique(data$portfolio_name_ref_all)
+  start_index <- which(data$status == "running")
+  done_index <- which(data$status == "done")
+  if (!identical(done_index, integer())){
+    start_time <- min(data$timestamp[start_index], na.rm = TRUE)
+    done_time <- max(data$timestamp[done_index], na.rm = TRUE)
+    runtime <- as.numeric(done_time - start_time)
+  } else {
+    runtime <- NA_integer_
+  }
+  out <- data.frame(
+    relpath = relpath,
+    portfolio_name_ref_all = portfolio_name_ref_all,
+    runtime = as.numeric(runtime)
   )
-  DBI::dbDisconnect(con)
-  messages <- parse_queue_message(queue_table[["message"]])
-  messages[["queue_status"]] <- queue_table[["status"]]
-  return(messages)
+  return(out)
+}
+
+current_runners <- function(data){
+  registered <- data[data$status == "register", c("worker", "pid")]
+  deregistered <- data[data$status == "deregister", c("worker", "pid")]
+  out <- setdiff(registered, deregistered)
+  return(out)
+}
+
+get_queue_stats <- function(queue){
+  waiting <- queue$count()
+  supplemental <- read_supplemental(queue, skip = 0, n = Inf)
+  supplemental$timestamp <- as.POSIXct(supplemental$timestamp)
+  runtimes <- do.call(rbind, by(
+    data = supplemental,
+    INDICES = list(supplemental$relpath, supplemental$portfolio_name_ref_all),
+    FUN = find_runtime,
+    simplify = FALSE
+    ))
+  avg_runtime <- mean(runtimes$runtime, na.rm = TRUE)
+  runners <- current_runners(supplemental)
+  num_runners <- nrow(runners)
+  browser()
 }
